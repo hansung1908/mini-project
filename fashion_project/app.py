b4a9078cdfab00ddcd389f9e4a6f646cfa8ca5a4
hansung1908 <hansung1908@naver.com>
2024-03-07 21:58:33 +0900

    화면 구성 수정

--- fashion_project/app.py
+++ fashion_project/app.py
@@ -11,13 +11,13 @@ def index():
 @app.route('/submit', methods=['POST'])
 def submit():
     location = request.form['location']
-    return redirect(url_for('view', location = location))
+    return redirect(url_for('view', location=location))
 
 @app.route("/view")
 def view():
     location = request.args.get('location')
     weather_info = weather.weather_data.get(location, "날씨 정보를 찾을 수 없습니다.")
-    return f"지역: {location}<br>기온: {weather_info['기온']}℃<br>강수량: {weather_info['강수량']}mm<br>풍속: {weather_info['풍속']}m/s"
+    return render_template('view.html', location=location, weather_info=weather_info)
 
 
 app.run(debug=True)
