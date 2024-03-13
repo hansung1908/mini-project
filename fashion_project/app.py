d79179482b8cc4ca04fa68317c7bbdc726d24dc2
hansung1908 <hansung1908@naver.com>
2024-03-13 19:55:06 +0900

    기온별 사진 출력 추가

--- fashion_project/app.py
+++ fashion_project/app.py
@@ -19,5 +19,4 @@ def view():
     weather_info = weather.weather_data.get(location, "날씨 정보를 찾을 수 없습니다.")
     return render_template('view.html', location=location, weather_info=weather_info)
 
-
 app.run(debug=True)
